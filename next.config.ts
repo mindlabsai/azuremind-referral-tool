267059d1a5a7c7b606a2bf56dea7d9ea370f760d
mindlabsai <vishalm06@icloud.com>
2026-05-11 09:50:22 +0800

    Configure ESLint settings in Next.js to ignore errors during builds, addressing known issues with eslint-webpack-plugin.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /**
+   * Avoid eslint-webpack-plugin running inside webpack’s child compilation.
+   * Known breakage: `Cannot set properties of undefined (setting 'defaultMeta')`
+   * (ESLint / eslint-webpack-plugin / transitive logging deps after npm updates).
+   * Run `npm run lint` locally / in CI instead.
+   */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
